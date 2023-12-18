d635b63a09ebc30e48224e013a91812d1d025e71
arsam-03 <ashahriary03@gmail.com>
2023-12-17 21:15:27 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+let currentTempCelsius;
+let currentTempFahrenheit;
+let isCelsius = true;
+let localTimeEpoch;
+
+async function getWeather() {
+    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=18d380a0716d4208ab8233016231712&q=Barcelona&aqi=no")
+    const data = await response.json()
+    console.log(data)
+
+    currentTempCelsius = data.current.temp_c;
+    currentTempFahrenheit = data.current.temp_f;
+
+    localTimeEpoch = data.location.localtime_epoch;
+
+    document.getElementById('temperature').innerText = `${currentTempCelsius}°C`;
+    updateLocalTime();
+}
+
+function updateLocalTime(epoch) {
+    const localDateTime = new Date(localTimeEpoch * 1000); 
+    const formattedTime = formatLocalTime(localDateTime);
+    document.getElementById('localTime').innerText = formattedTime;
+
+    localTimeEpoch += 1;
+}
+
+
+function formatLocalTime(dateTime) {
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZone: 'Europe/Madrid',
+        timeZoneName: 'short'
+    };
+    return dateTime.toLocaleDateString('en-US', options)
+}
+
+
+window.onload = function() {
+    getWeather();
+    setInterval(updateLocalTime, 1000);
+};
+
+function toggleTemperature() {
+    if (isCelsius) {
+        document.getElementById('temperature').innerText = `${currentTempFahrenheit}°F`;
+    } else {
+        document.getElementById('temperature').innerText = `${currentTempCelsius}°C`;
+    }
+    isCelsius = !isCelsius; 
+}
